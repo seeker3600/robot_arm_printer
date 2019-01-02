b23171220c5e31d7743e276981e5499a8f7b014c
seeker3600 <seeker3600@zoho.com>
2019-01-02 16:54:46 +0900

    ある程度滑らかに大体直線運動を実現。

--- robot_arm_printer.ino
+++ robot_arm_printer.ino
@@ -1,15 +1,6 @@
 #include <VarSpeedServo.h>
 #include <FABRIK2D.h>
 
-#define MOVE_UNIT (2.0f)
-#define SERVO_SPEED (10)
-
-VarSpeedServo s[6];
-
-//int lengths[] = {120, 125}; //形状的にはこちらが近い
-int lengths[] = {150, 105}; //構造による補正値
-Fabrik2D fabrik2D(3, lengths);
-
 /*
 s2とs3の角度を制限する。
 r2: s2の角度。
@@ -17,7 +8,8 @@ r3: s3の角度。
 r3current: s3の現在の角度。
            省略すると、s3の角度は制限せず、s2のみを制限する。
 */
-void constrainS2S3(float& r2, float& r3, float r3current = -1)
+template<typename T>
+void constrainS2S3(T& r2, T& r3, T r3current = -1)
 {
   if(r3current >= 0 && r3 > r3current)
   {
@@ -33,12 +25,75 @@ void constrainS2S3(float& r2, float& r3, float r3current = -1)
 /*
 FABRIK2Dのモデルをロボットアームのサーボモータの角度に変換する。
 */
-void convert(float& r2, float& r3)
+template<typename T>
+void convert(T& r2, T& r3)
 {
   r2 = -r2 - r3;
 }
 
+//int lengths[] = {120, 125}; //形状的にはこちらが近い
+int lengths[] = {150, 105}; //構造による補正値
+Fabrik2D fabrik2D(3, lengths);
+
+void move(float x, float y, float& r2, float& r3)
+{
+  fabrik2D.solve(x, y, lengths);
+
+  r3 = fabrik2D.getAngle(0) * 57296.0f / 1000.0f;
+  r2 = fabrik2D.getAngle(1) * 57296.0f / 1000.0f;
+  convert(r2, r3);
+  constrainS2S3(r2, r3);
+}
+
+#define Y_MIN (-10.0f)
+#define Y_MAX ( 70.0f)
+#define STEP (80)
+#define S_PER_STEP (0.03125f)
+
+servoSequencePoint s2seq[STEP];
+servoSequencePoint s3seq[STEP];
+
+servoSequencePoint resetseq[2] = {{90, 10}, {91, 10}};
+
+uint8_t calcSpeed(float r0, float r)
+{
+  float sa = r0 - r;
+  int speed = abs(sa) / (2.2809 * S_PER_STEP) - 0.16765;
+  return constrain(speed, 1, 255);
+}
+
+void initSeq()
+{
+  const float unit = (Y_MAX - Y_MIN) / (float)STEP;
+  float y = Y_MIN;
+  float r2, r3, r20, r30;
+
+  for(int n = 0; n < STEP; ++n)
+  {
+    move(80.0f, y, r2, r3);
+
+    uint8_t s2speed = n > 0 ? calcSpeed(r20, r2) : 10;
+    uint8_t s3speed = n > 0 ? calcSpeed(r30, r3) : 10;
+
+    s2seq[n] = {(uint8_t)r2, s2speed};
+    s3seq[n] = {(uint8_t)r3, s3speed};
+
+    Serial.print(n);
+    Serial.print(": r2 = \t");
+    Serial.print(s2seq[n].position);
+    Serial.print(", r3 = \t");
+    Serial.println(s3seq[n].position);
+
+    y += unit;
+    r20 = r2;
+    r30 = r3;
+  }
+}
+
+VarSpeedServo s[6];
+
 void setup() {
+  Serial.begin(9600);
 
   s[0].attach(2);
   s[1].attach(3);
@@ -47,63 +102,41 @@ void setup() {
   s[4].attach(6, 20, 160);
   s[5].attach(7);
 
+  pinMode(8, INPUT_PULLUP);
+
   fabrik2D.setTolerance(0.5);
-
-  //pinMode(8, INPUT_PULLUP);
-  Serial.begin(9600);
+  initSeq();
 }
 
-bool wrote = false;
-
-void move(float x, float y)
+void reset()
 {
-  fabrik2D.solve(x, y, lengths);
+  s[1].write(s2seq[0].position, 20);
+  s[2].write(s3seq[0].position, 20);
 
-  float r3 = fabrik2D.getAngle(0) * 57296.0f / 1000.0f;
-  float r2 = fabrik2D.getAngle(1) * 57296.0f / 1000.0f;
-  convert(r2, r3);
+  s[1].wait();
+  s[2].wait();
 
-  Serial.print("X = ");
-  Serial.print(x);
-  Serial.print(", Y = ");
-  Serial.print(y);
-  Serial.print(", R2 = ");
-  Serial.print(r2);
-  Serial.print(", R3 = ");
-  Serial.println(r3);
-
-  constrainS2S3(r2, r3, s[2].read());
-
-  VarSpeedServo* s2 = &s[1];
-  VarSpeedServo* s3 = &s[2];
-
-  if(wrote) s2->wait();
-  if(wrote) s3->wait();
-  s2->write(s2->toMicroseconds(r2), SERVO_SPEED, false);
-  s3->write(s3->toMicroseconds(r3), SERVO_SPEED, false);
-  wrote = true;
+  delay(1000);
 }
 
-float y = 0;
-int mode = 0;
-
-void position()
+void play()
 {
-  switch(mode)
+  for(int n = 1; n < STEP; ++n)
   {
-    case 0:
-      y -= MOVE_UNIT;
-      if(y == -10.0f) ++mode;
-      break;
-    case 1:
-      y += MOVE_UNIT;
-      if(y == 80.0f) mode = 0;
-      break;
+    s[1].write(s2seq[n].position, s2seq[n].speed, false);
+    s[2].write(s3seq[n].position, s3seq[n].speed, false);
+
+    while(s[1].isMoving() || s[2].isMoving()) { delay(1); }
   }
+
+  delay(1000);
 }
 
 void loop() {
   //return;
-  position();
-  move(80.0f, y);
+
+  reset();
+  play();
+
+  while(digitalRead(8) == HIGH);
 }
